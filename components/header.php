d7a0c5a3dc9ddfc95a0caf78e787c7117c10dd7b
Aroop 'Finlay' Roelofs <me@finlaydag33k.nl>
2017-04-26 10:38:35 +0200

    Bugfix
    Fixed a bug where going to somebodies profile would result in a layout-less page

--- components/header.php
+++ components/header.php
@@ -1,14 +1,14 @@
-<link rel="stylesheet" href="//<?= $dir; ?>/inc/css/bootstrap.min.css" />
-<link rel="stylesheet" href="//<?= $dir; ?>/inc/css/assets.css" />
-<script src="//<?= $dir; ?>/inc/js/jquery.min.js"></script>
-<script src="//<?= $dir; ?>/inc/js/bootstrap.min.js"></script>
-<script src="//<?= $dir; ?>/inc/js/jquery.cookie.js"></script>
-<script src="//<?= $dir; ?>/inc/js/jquery.tablesorter.min.js"></script>
+<link rel="stylesheet" href="//<?= $server; ?>/inc/css/bootstrap.min.css" />
+<link rel="stylesheet" href="//<?= $server; ?>/inc/css/assets.css" />
+<script src="//<?= $server; ?>/inc/js/jquery.min.js"></script>
+<script src="//<?= $server; ?>/inc/js/bootstrap.min.js"></script>
+<script src="//<?= $server; ?>/inc/js/jquery.cookie.js"></script>
+<script src="//<?= $server; ?>/inc/js/jquery.tablesorter.min.js"></script>
 
-<link rel="stylesheet" href="//<?= $dir; ?>/inc/css/select2.css" />
-<script src="//<?= $dir; ?>/inc/js/select2.min.js"></script>
-<script src="//<?= $dir; ?>/inc/js/bootstrap.notify.js"></script>
-<link rel="stylesheet" href="//<?= $dir; ?>/inc/css/animate.css" />
+<link rel="stylesheet" href="//<?= $server; ?>/inc/css/select2.css" />
+<script src="//<?= $server; ?>/inc/js/select2.min.js"></script>
+<script src="//<?= $server; ?>/inc/js/bootstrap.notify.js"></script>
+<link rel="stylesheet" href="//<?= $server; ?>/inc/css/animate.css" />
 
 <!-- Google is watching you! -->
 <script src='https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit' async defer></script>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	$url = $_SERVER['REQUEST_URI']; //returns the current URL
 	$parts = explode('/',$url);
 	$dir = $_SERVER['SERVER_NAME'];
+	$server = $_SERVER['SERVER_NAME'];
 	for ($i = 0; $i < count($parts) - 1; $i++) {
 	 $dir .= $parts[$i] . "/";
 	}
